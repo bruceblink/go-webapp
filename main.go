11f0da0cdb17977035a00ab313e80cce82d52e45
bruceblink <likanug.g@gmail.com>
2026-05-24 14:08:25 +0800

    feat: wait goroutine in main function

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
 	fmt.Printf("main function started \n")
 	CalcStoreTotal(Products)
+	time.Sleep(time.Second * 5)
 	fmt.Printf("main function complete")
 }
 
@@ -21,5 +25,14 @@ main function complete
 /* after add goroutine in CalcStoreTotal function
 main function started
 Total: $0.00
+main function complete
+*/
+
+/* wait goroutine in main function
+main function started
+Total: $0.00
+Running subtotal: $79595.00
+Soccer subtotal: $54.45
+Watersports subtotal: $323.95
 main function complete
 */

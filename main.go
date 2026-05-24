be0791e2148fc87b12d19126033f396882eaab90
bruceblink <likanug.g@gmail.com>
2026-05-24 13:57:25 +0800

    chore: add output example

--- main.go
+++ main.go
@@ -7,3 +7,13 @@ func main() {
 	CalcStoreTotal(Products)
 	fmt.Printf("main function complete")
 }
+
+// output
+/*
+main function started
+Soccer subtotal: $54.45
+Running subtotal: $79595.00
+Watersports subtotal: $323.95
+Total: $79973.40
+main function complete
+*/

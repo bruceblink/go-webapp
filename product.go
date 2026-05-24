2f63c8da6dd15c53851c7e6125ce14b4ef8df151
bruceblink <likanug.g@gmail.com>
2026-05-24 11:30:51 +0800

    初始化项目，添加 go.mod 和 product.go 文件

---
+++ product.go
@@ -0,0 +1,28 @@
+package main
+
+type Product struct {
+	Name, Category string
+	Price          float64
+}
+
+var products = []*Product{
+	{"Kayak", "Watersports", 275},
+	{"Lifejacket", "Watersports", 48.95},
+	{"Soccer Ball", "Soccer", 19.50},
+	{"Corner Flags", "Soccer", 34.95},
+	{"Running Shoes", "Running", 95.00},
+	{"Stadium", "Running", 79500.00},
+}
+
+type ProductGroup []*Product
+
+type ProductData = map[string]ProductGroup
+
+var Products = make(ProductData)
+
+func init() {
+	for _, p := range products {
+		Products[p.Category] = append(Products[p.Category], p)
+
+	}
+}
